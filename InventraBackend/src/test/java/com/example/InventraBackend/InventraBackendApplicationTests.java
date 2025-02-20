330d8d3a502f7639128db6b0df9a51aeadaa1845
eymenefealtun <eymenefealtun@hotmail.com>
2025-02-20 15:25:58 +0300

    feat: backend base created

---
+++ InventraBackend/src/test/java/com/example/InventraBackend/InventraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InventraBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
